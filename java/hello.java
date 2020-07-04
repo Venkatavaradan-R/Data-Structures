f0e277bc5de26546fb5ce1a19f4edfabf632ccdc
venkat <venkatavaradan.r@gmail.com>
2020-07-04 21:56:40 +0530

    class hi

--- java/hello.java
+++ java/hello.java
@@ -1,3 +1,9 @@
+class hi {
+    public static void main(String[] args) {
+        hello.main();
+    }
+}
+
 class hello {
     public static void main(String[] args) {
         System.out.println("Hello my nigga");
