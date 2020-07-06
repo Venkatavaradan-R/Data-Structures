b82cd81a2ce7ca9ef89e6d523a48279188ebe6bb
venkat <venkatavaradan.r@gmail.com>
2020-07-06 16:37:20 +0530

    javaprac

--- java/hello.java
+++ java/hello.java
@@ -2,5 +2,8 @@ class hello {
   public static void main(String[] args) {
     int num = 100;
     System.out.println("cac" + num);
+    int minint = Integer.MAX_VALUE;
+    System.out.println("min int" + minint);
+
   }
 }
