e076029134b00a51500c519df4640f2d6eff6c6e
venkat <venkatavaradan.r@gmail.com>
2020-07-10 09:40:11 +0530

    java if else and expression abbreviations

--- java/hello.java
+++ java/hello.java
@@ -6,7 +6,13 @@ class hello {
     System.out.println("val in pound:" + poundval);
     char uni = '\u0069';
     System.out.println("val in unicode val:" + uni);
-    String test = "my wight is " + kgval + " KGs";
+    String test = "my weight is " + kgval + " KGs";
     System.out.println("string val: " + test);
+
+    boolean is_gay = true;
+
+    if (is_gay)
+      System.out.println("he is gay? " + is_gay);
+
   }
 }
