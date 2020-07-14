7eee3590b80a8aa61055354873161bd9800ccf4d
venkat <venkatavaradan.r@gmail.com>
2020-07-14 20:03:14 +0530

    cpp/ll structs

---
+++ cpp/LinkedList.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+typedef struct node node;
+
+struct LinkedList
+{
+    int non;
+    node *head;
+};
+
+int main()
+{
+}
