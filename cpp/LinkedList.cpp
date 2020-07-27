19ac7e9d61d7e1f9c67d6c151c9ad31f6c622f34
venkat <venkatavaradan.r@gmail.com>
2020-07-27 20:06:57 +0530

    display

--- cpp/LinkedList.cpp
+++ cpp/LinkedList.cpp
@@ -22,22 +22,13 @@ using namespace std;
 class node
 {
 public:
-    int data;
-    struct node *link;
-    node(int x = 0, struct node *y = NULL)
-    {
-        data = x;
-        link = y;
-    }
-    // Not needed because of constructor
-
-    // node* create_node(int data)
-    // {
-    //   node *temp = new node;
-    //   temp->data  = data;
-    //   temp->link = NULL;
-    //   return temp;
-    // }
+  int data;
+  struct node *link;
+  node(int x = 0, struct node *y = NULL)
+  {
+    data = x;
+    link = y;
+  }
 };
 
 typedef struct node node;
@@ -45,38 +36,49 @@ typedef struct node node;
 class LinkedList
 {
 public:
-    int non;
-    node *head;
+  int non;
+  node *head;
 
-    LinkedList()
+  LinkedList()
+  {
+    non = 0;
+    head = NULL;
+  }
+
+  void insert_front(int data)
+  {
+    node *temp = new node(data);
+
+    if (non == 0)
     {
-        non = 0;
-        head = NULL;
+      head = temp;
+      non++;
+      return;
     }
 
-    void insert_front(int data)
+    temp->link = head;
+    head = temp;
+    non++;
+    return;
+  }
+  void display()
+  {
+    if (non == 0)
+      return;
+
+    if (non == 1)
     {
-      temp = new node(data);
-
-      if (non == 0)
-      {
-        head = temp;
-        non++;
-        return;
-      }
-
-      if (non==1)
-      {
-        temp->link = head;
-        head = temp;
-        non++;
-        return;
-      }
-
-      // if 
-
-
+      cout << head->data << " " << endl;
     }
+
+    node *p = head, *q = NULL;
+
+    while (p != NULL)
+    {
+      cout << p->data << " ";
+    }
+    cout << endl;
+  }
 };
 
 // node* create_node(int);
@@ -85,11 +87,13 @@ typedef struct LinkedList LL;
 
 int main()
 {
-    LL *list1 = new LL;
-    node *n1 = new node(100);
-    node *n2 = new node(NULL, n1);
-    cout << "node1: " << n1->data << "  " << n1->link << endl;
-    cout << "node2: " << n2->data << "  " << n2->link << endl;
+  LL *list1 = new LL;
+  node *n1 = new node(100);
+  node *n2 = new node(NULL, n1);
+  cout << "node1: " << n1->data << "  " << n1->link << endl;
+  cout << "node2: " << n2->data << "  " << n2->link << endl;
 
-    delete list1;
+  list1->insert_front(10);
+
+  delete list1;
 }
